5734fd7f38ee43b717a535547f17660dbf84be7a
Dmitriy Korolov <adrorecords@gmail.com>
2021-11-10 10:22:53 +0200

    hw-10

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+import '../css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './services/fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+
+const searchbox = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+searchbox.addEventListener('input', debounce(() => {
+    fetchCountries(searchbox.value)
+      .then(userdata => showCountries(userdata))
+      .catch(error => showError(error));
+  }, DEBOUNCE_DELAY));
+
+
+function showCountries(counrtries) {
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
+
+     if (counrtries.length > 10) {
+     return Notify.success("Too many matches found. Please enter a more specific name.");
+    }
+
+    if ((counrtries.length >= 2) && (counrtries.length <= 10)) {
+        const markup = counrtries.map(item => 
+            `<li>
+            <img src="${item.flags.svg}" class="avatar" alt="flag" width="30" />
+            <span>${item.name.official}</span></li>`
+        ).join('');
+        return countryListEl.insertAdjacentHTML('beforeend', markup);
+    }
+    const country = counrtries[0];
+    const markup = `<img src="${country.flags.svg}" class="avatar" alt="flag" width="30" />
+            <span class = 'title'>${country.name.official}</span>
+            <ul class = 'text'>
+            <li>Capital: <span class = 'text__normal'>${country.capital}</span></li>
+            <li>Population: <span class = 'text__normal'>${country.population}</span></li>
+            <li>Languages: <span class = 'text__normal'>${Object.keys(country.languages).join(', ')}</span></li>
+            </ul>`;
+    
+    return countryInfoEl.insertAdjacentHTML('beforeend', markup);
+}
+
+function showError() {
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
+  return Notify.failure("Oops, there is no country with that name");
+}
---
+++ src/js/services/fetchCountries.js
@@ -0,0 +1,11 @@
+export function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+// ?fields=name.official,capital,population,flags.svg,languages`)
